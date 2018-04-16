7824274954939c53fc8bb59f1e6ac83480da7920
劉雨辰 <liu0101@r.recruit.co.jp>
2018-04-16 17:32:14 +0900

    函数指针

--- learnc/learnc/main.c
+++ learnc/learnc/main.c
@@ -11,6 +11,12 @@
 #define NUM1 10
 #define NUM2 20
 
+//返回两个数中较大的一个
+int max(int a, int b){
+    return a>b ? a : b;
+}
+
+
 int main(int argc, const char * argv[]) {
     int a1=20, a2=345, a3=700, a4=22;
     int b1=56720, b2=9999, b3=20098, b4=2;
@@ -23,15 +29,15 @@ int main(int argc, const char * argv[]) {
     printf("%-9d %-9d %-9d %-9d\n", c1, c2, c3, c4);
     printf("%-9d %-9d %-9d %-9d\n", d1, d2, d3, d4);
     
-    int a[4] = {20, 345, 700, 22};
-    int b[4] = {56720, 9999, 20098, 2};
-    int c[4] = {233, 205, 1, 6666};
-    int d[4] = {34, 0, 23, 23006783};
-
-    printf("%-9d %-9d %-9d %-9d\n", a[0], a[1], a[2], a[3]);
-    printf("%-9d %-9d %-9d %-9d\n", b[0], b[1], b[2], b[3]);
-    printf("%-9d %-9d %-9d %-9d\n", c[0], c[1], c[2], c[3]);
-    printf("%-9d %-9d %-9d %-9d\n", d[0], d[1], d[2], d[3]);
+//    int a[4] = {20, 345, 700, 22};
+//    int b[4] = {56720, 9999, 20098, 2};
+//    int c[4] = {233, 205, 1, 6666};
+//    int d[4] = {34, 0, 23, 23006783};
+//
+//    printf("%-9d %-9d %-9d %-9d\n", a[0], a[1], a[2], a[3]);
+//    printf("%-9d %-9d %-9d %-9d\n", b[0], b[1], b[2], b[3]);
+//    printf("%-9d %-9d %-9d %-9d\n", c[0], c[1], c[2], c[3]);
+//    printf("%-9d %-9d %-9d %-9d\n", d[0], d[1], d[2], d[3]);
     
 //    // 对单个元素赋值
 //    int a[3];
@@ -57,6 +63,22 @@ int main(int argc, const char * argv[]) {
     //代码B
     printf("Error\n");
 #endif
+    
+    int a = 16, b = 932, c = 100;
+    //定义一个指针数组
+    int *arr[3] = {&a, &b, &c};//也可以不指定长度，直接写作 int *parr[]
+    //定义一个指向指针数组的指针
+    int **parr = arr;
+    printf("%d, %d, %d\n", *arr[0], *arr[1], *arr[2]);
+    printf("%d, %d, %d\n", **(parr+0), **(parr+1), **(parr+2));
+    
+    int x, y, maxval;
+    //定义函数指针
+    int (*pmax)(int, int) = max;  //也可以写作int (*pmax)(int a, int b)
+    printf("Input two numbers:");
+    scanf("%d %d", &x, &y);
+    maxval = (*pmax)(x, y);
+    printf("Max value: %d\n", maxval);
     
     return 0;
 }
